e0c8fdb82b054e913157daa15dd72fe4bb7498eb
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-05-10 08:57:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/debug/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.debug
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val list = ArrayList<String>()
+        list.add ("おはようございます")
+        list.add("こんにちは！")
+
+                textView.text = list[2]
+
+    }
+}
